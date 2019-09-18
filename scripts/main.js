7794167388be5465dd3e422c4503ce52ff9961f6
Dmitry Kim <kimdimka@yandex.ru>
2019-09-18 13:00:14 +1000

    Сделаны основное меню и менеджер игры

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,6 @@ mainCanvasZ2.width = window.innerWidth;
 mainCanvasZ2.height = window.innerHeight;
 var mainContextZ2 = mainCanvasZ2.getContext("2d");
 
-
 function bgImageObject(src){
     this._src = src;
     this._bg;
@@ -17,20 +16,23 @@ function bgImageObject(src){
     let ctxZ2 =mainContextZ2;
 
     this.createImage = function(){
-        this._isLoaded = false;
-        this._bg = new Image(1920,1080);
-        this._bg.src = this._src;
-        this._bg.onload = this.repaintedCanvas.apply(this);
-        return this;
-    }
+        return new Promise(res=>{
+            this._isLoaded = false;
+            this._bg = new Image(1920,1080);
+            this._bg.src = this._src;
+            this._bg.onload = ()=> {
+                this.repaintedCanvas.apply(this);
+                res(this);
+            };
+            });
+
+        };
     this.draw = function(x,y){
         ctxZ1.drawImage(this._bg,x,y,mainCanvasZ1.width,mainCanvasZ1.height);
     }
     this.repaintedCanvas = function(){
         this._isLoaded = true;
     }
-
-
 }
 function gameManagerObject(){
     this.goblinObject = {
@@ -44,7 +46,6 @@ function gameManagerObject(){
         states:{angry:[],happy:[]},
         loadImages:function(){
             return new Promise(res=>{
-
                 this.states = {angry:[],happy:[]};
                 this.angryAnim.forEach((el)=>{
                     let img =new Image(this.sizeX,this.sizeY);
@@ -63,14 +64,14 @@ function gameManagerObject(){
         },
 
         drawCalmStateGoblin:function (x,y,ctxZ1,ctxZ2) {
-            ctxZ2.clearRect(0,0,mainCanvasZ2.width,mainCanvasZ2.height);
+            ctxZ2.clearRect(x,y,mainCanvasZ2.width,mainCanvasZ2.height);
             ctxZ2.drawImage(this.states['happy'][0],x,y,this.width,this.height);
         },
         animHappyStateGoblin:function (x,y,ctxZ1,ctxZ2) {
             let i=0;
             var int = setInterval(function(){
                 if (i==this.states['happy'].length-1) clearInterval(int);
-                ctxZ2.clearRect(0,0,mainCanvasZ2.width,mainCanvasZ2.height);
+                ctxZ2.clearRect(x,y,mainCanvasZ2.width,mainCanvasZ2.height);
                 ctxZ2.drawImage(this.states['happy'][i],x,y,this.width,this.height);
                 i++;
             }.bind(this),100);
@@ -79,85 +80,145 @@ function gameManagerObject(){
             let i=0;
             var int = setInterval(function(){
                 if (i==this.states['angry'].length-1) clearInterval(int);
-                ctxZ2.clearRect(0,0,mainCanvasZ2.width,mainCanvasZ2.height);
+                ctxZ2.clearRect(x,y,mainCanvasZ2.width,mainCanvasZ2.height);
                 ctxZ2.drawImage(this.states['angry'][i],x,y,this.width,this.height);
                 i++;
             }.bind(this),100);
         }
 
 
-
-
-
     }
-
-
-
-
-    const progressImageSize = {x:512,y:82};
+    const progressImageSize = {x:412,y:82};
     const scoreImageSize = {x:241,y:91};
-
+    const btnImageSize = {x:150,y:70};
     this._isLoaded;
     this._playerTitle;
     this._taskTitle;
     this._scoreTitle;
     this._progressFilled;
-
-    this._progress;
+    this._progress =[];
     this._score;
     this.start;
-    this.createImage = function(){
-        this._playerTitle = "Коля";
-        this._taskTitle = "Задание 1";
-        this._isLoaded = false;
-        this._progress = new Image(512,82);
-        this._score = new Image(241,91);
-        this._progressFilled = 0;
-        this._progressFilledTitle = "4 из 11";
-        this._scoreTitle = 50;
-        let loaded = function(){
-            this._score.src = "./images/elements/score.png";
-            this._score.onload = this.repaintedCanvas.apply(this);
-        };
+    this._yesBtn;
+    this._noBtn;
+    this._yesBtnp;
+    this._noBtnp;
+    let x1YesBtn = -btnImageSize.x/2+mainCanvasZ2.width/2-150;
+    let y1YesBtn = +mainCanvasZ2.height-100;
+    let x2YesBtn = -btnImageSize.x/2+mainCanvasZ2.width/2-150+btnImageSize.x;
+    let y2YesBtn = +mainCanvasZ2.height-100+btnImageSize.y;
+    let x1NoBtn = +btnImageSize.x/2+mainCanvasZ2.width/2-150;
+    let y1NoBtn = +mainCanvasZ2.height-100;
+    let x2NoBtn = +btnImageSize.x/2+mainCanvasZ2.width/2-150+btnImageSize.x;
+    let y2NoBtn = +mainCanvasZ2.height-100+btnImageSize.y;
 
-        this._progress.src = "./images/elements/pb-5.png";
-        this._progress.onload = loaded.apply(this);
-        this.start=true;
-        return this;
+    this.createImage = function(){
+        return new Promise((res)=>{
+            this._playerTitle = "Коля";
+            this._taskTitle = "Задание 1";
+            this._isLoaded = false;
+            this._score = new Image(scoreImageSize.x,scoreImageSize.y);
+            this._yesBtn = new Image(btnImageSize.x,btnImageSize.y);
+            this._noBtn = new Image(btnImageSize.x,btnImageSize.y);
+            this._yesBtnp = new Image(btnImageSize.x,btnImageSize.y);
+            this._noBtnp = new Image(btnImageSize.x,btnImageSize.y);
+            this._btnYesPressed;
+            this._btnNoPressed;
+            this._progressFilled = 0;
+            this._progressFilledTitle = `${this._progressFilled} из 11`;
+            this._scoreTitle = 50;
+            this._pbArr = ['pb-1.png','pb-2.png','pb-3.png','pb-4.png','pb-5.png','pb-6.png','pb-7.png','pb-8.png','pb-9.png','pb-10.png'];
+            let loaded = function(){
+                this._score.src = "./images/elements/score.png";
+                this._score.onload = ()=>{
+                    this.repaintedCanvas.apply(this);res(this);
+                }
+            };
+            this._yesBtn.src = './images/elements/btnYes.png';
+            this._noBtn.src = './images/elements/btnNo.png';
+            this._yesBtnp.src = './images/elements/btnYesp.png';
+            this._noBtnp.src = './images/elements/btnNop.png';
+
+            this._pbArr.forEach((el)=>{
+                let img = new Image(progressImageSize.x,progressImageSize.y);
+                img.src = './images/elements/'+el;
+                this._progress.push(img);
+                if (el=='pb-10.png')
+                    img.onload = loaded.apply(this);
+            });
+            this.start=true;
+
+        });
+
     }
     this.drawInfo = function (x,y,ctxZ1,ctxZ2) {
-        ctxZ2.clearRect(0,0,mainCanvasZ2.width,mainCanvasZ2.height/2);
         ctxZ1.fillStyle = "darkviolet";
         ctxZ1.font = "bold 30px Comic Sans MS";
         ctxZ1.fillText(this._playerTitle, 30, 50);
-        ctxZ1.fillText(this._taskTitle, 550, 50);
-        ctxZ1.drawImage(this._progress,x-progressImageSize.x+mainCanvasZ1.width-40,y,progressImageSize.x,progressImageSize.y);
+        ctxZ1.fillText(this._taskTitle, mainCanvasZ1.width/2-100, 50);
+        ctxZ1.drawImage(this._progress[this._progressFilled],x-progressImageSize.x+mainCanvasZ1.width-40,y+10,progressImageSize.x,progressImageSize.y);
         ctxZ1.drawImage(this._score,x+150,y,scoreImageSize.x,scoreImageSize.y);
+        ctxZ2.clearRect(0,0,mainCanvasZ2.width,progressImageSize.y+100);
         ctxZ2.fillStyle = "brown";
         ctxZ2.font = "bold 30px Comic Sans MS";
-        ctxZ2.fillText(this._scoreTitle.toString(), 270, 55);
+        ctxZ2.fillText(this._scoreTitle.toString(), scoreImageSize.x/2+150, 55);
         ctxZ2.fillStyle = "brown";
         ctxZ2.font = "bold 30px Comic Sans MS";
-        ctxZ2.fillText(this._progressFilledTitle, mainCanvasZ2.width-370, 55);
+        ctxZ2.fillText(this._progressFilledTitle, mainCanvasZ2.width-progressImageSize.x/2-100, 55);
+
+
+        ctxZ2.drawImage(this._btnYesPressed?this._yesBtnp:this._yesBtn,x1YesBtn,y1YesBtn,btnImageSize.x,btnImageSize.y);
+        ctxZ2.drawImage(this._btnNoPressed?this._noBtnp:this._noBtn ,x+btnImageSize.x/2+mainCanvasZ2.width/2-150,y+mainCanvasZ2.height-100,btnImageSize.x,btnImageSize.y);
+
+
+
     }
 
+    this.drawOnce = function (x,y,ctxZ1,ctxZ2) {
+        this._btnYesPressed = false;
+        this._btnNoPressed = false;
 
+        this.goblinObject.loadImages().then((s)=>{
+            this.goblinObject.drawCalmStateGoblin(x+mainCanvasZ2.width-this.goblinObject.width,y+mainCanvasZ2.height-this.goblinObject.height,ctxZ1,ctxZ2);
+            //this.goblinObject.animHappyStateGoblin(x+mainCanvasZ2.width-this.goblinObject.width,y+mainCanvasZ2.height-this.goblinObject.height,ctxZ1,ctxZ2);
+            this.goblinObject.animAngryStateGoblin(x+mainCanvasZ2.width-this.goblinObject.width,y+mainCanvasZ2.height-this.goblinObject.height,ctxZ1,ctxZ2);
+        });
+        this.start=false;
+
+        mainCanvasZ2.addEventListener('click',(target)=>{
+            let x= target.x;
+            let y= target.y;
+            if (x>=x1YesBtn&&x<x2YesBtn&&y>y1YesBtn&&y<y2YesBtn){
+                this._btnYesPressed = true;
+                setTimeout(()=>this._btnYesPressed = false,200);
+
+            }
+            if (x>=x1NoBtn&&x<x2NoBtn&&y>y1NoBtn&&y<y2NoBtn){
+                this._btnNoPressed = true;
+                setTimeout(()=>this._btnNoPressed = false,200);
+            }
+        });
+        mainCanvasZ2.addEventListener('mousemove',(target)=>{
+            let x= target.x;
+            let y= target.y;
+            if (x>=x1YesBtn&&x<x2YesBtn&&y>y1YesBtn&&y<y2YesBtn){
+                document.body.style.cursor = "pointer";
+
+            } else if (x>=x1NoBtn&&x<x2NoBtn&&y>y1NoBtn&&y<y2NoBtn){
+                document.body.style.cursor = "pointer"
+            } else document.body.style.cursor = "default"
+        });
+    }
 
     this.draw = function(x,y){
         let ctxZ1 =mainContextZ1;
         let ctxZ2 =mainContextZ2;
-
         this.drawInfo(x,y,ctxZ1,ctxZ2);
-
         if (this.start==true){
-            this.goblinObject.loadImages().then((s)=>{
-                this.goblinObject.drawCalmStateGoblin(x+mainCanvasZ2.width-this.goblinObject.width,y+mainCanvasZ2.height-this.goblinObject.height,ctxZ1,ctxZ2);
-                //this.goblinObject.animHappyStateGoblin(x+mainCanvasZ2.width-this.goblinObject.width,y+mainCanvasZ2.height-this.goblinObject.height,ctxZ1,ctxZ2);
-                this.goblinObject.animAngryStateGoblin(x+mainCanvasZ2.width-this.goblinObject.width,y+mainCanvasZ2.height-this.goblinObject.height,ctxZ1,ctxZ2);
-
-            });
-            this.start=false;
+            this.drawOnce(x,y,ctxZ1,ctxZ2);
         }
+
+        //this.goblinObject.animAngryStateGoblin(x+mainCanvasZ2.width-this.goblinObject.width,y+mainCanvasZ2.height-this.goblinObject.height,ctxZ1,ctxZ2);
 
     }
     this.repaintedCanvas = function(){
@@ -165,19 +226,32 @@ function gameManagerObject(){
     }
 
 }
-var f=1;
+
+
+function gameProcessObject(){
+
+
+}
+
 
 function main(){
     let bgImageOb = new bgImageObject("./images/bg/bg-5.jpg");
-    bgImageOb.createImage().draw(0,0);
+    bgImageOb.createImage().then((e)=>{
+        e.draw(0,0);
+    });
+    gProc = new gameProcessObject();
+
     let gMgrImageOb = new gameManagerObject();
-    gMgrImageOb.createImage().draw(0,0);
-    gameLoop(gMgrImageOb);
+    gMgrImageOb.createImage().then((e)=>{
+        e.draw(0,0);
+        gameLoop(gMgrImageOb);
+
+    });
 }
 main();
 
 function gameLoop(gMgrImageOb) {
-    gMgrImageOb.draw();
+    gMgrImageOb.draw(0,0);
 
     requestAnimationFrame(gameLoop.bind(this,gMgrImageOb));
 
